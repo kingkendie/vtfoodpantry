96c6633deb557476cd2b14ea75593f7e515f31bf
l3sg <l3gendary17@yahoo.com>
2026-04-05 15:37:26 -0400

    test22323

---
+++ firebase_config.py
@@ -0,0 +1,5 @@
+"""
+Firebase configuration module.
+Initializes the Firebase Admin SDK using credentials from environment variables
+or a service account key file. Provides access to Firestore.
+"""
---
+++ vendors.py
@@ -0,0 +1,7 @@
+"""
+Routes for managing vendors.
+Includes endpoints to manage where food is sourced from or distributed to.
+"""
+from flask import Blueprint
+
+vendors_bp = Blueprint('vendors', __name__)
