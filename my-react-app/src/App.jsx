c1e34944b9ddebf4d082d2be3ef4c5815e3c325d
Kingjaw <mondattam@gmail.com>
2026-04-06 14:33:45 -0400

    stuff

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,12 +4,25 @@ function App() {
 
   return (
     <>
-    
-    
-    
+      <NavBar />
     </>
   )
 }
+
+
+function NavBar() {
+
+  return (
+    <>
+      <nav className="navbar">
+        <a href="/index.html">
+          <img src="../public/VTPantry.png" style={{ height: "60px" }} /></a>
+      </nav>
+
+    </>
+  )
+}
+
 
 
 export default App
