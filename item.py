5cc9dc59342481a6285ee7119ba4a47176d66541
prestonc05 <prestonc05@vt.edu>
2026-04-09 16:52:15 -0400

    No more item.py

--- item.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, id, name, category, unit_type, default_vendor, default_price_per_unit):
-        self.id = id
-        self.name = name
-        self.category = category
-        self.unit_type = unit_type
-        self.default_vendor = default_vendor
-        self.default_price_per_unit = default_price_per_unit
